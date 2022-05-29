a5276c607fa7d9ef71423811e3ad89ae4215f793
Oberis dos Santos Nascimento <oberis2010@gmail.com>
2022-05-29 17:13:25 -0300

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 import config
@@ -17,4 +18,6 @@ def home():
     return 'Home'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8888',debug=True)
+    #app.run(host='0.0.0.0', port='8888',debug=True)
+    port  =  int ( os . environ . get ( "PORT" ,  5000 )) 
+    app . execute ( host = '0.0.0.0' ,  porta = port )
